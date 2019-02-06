624086757b9f4c77ce56327ef7c474636cf5213d
Mikael <mikaelutterback@outlook.com>
2019-02-06 14:47:43 +0100

    Sends GET and POST request successfully towards backend.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'; // To get access to VueResource
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.use(VueResource); // To be able to use VueResource
+// Question: what root can I use to test this?
+// Vue.http.options.root = 'http://localhost/'; //The root URL
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    port: 4200 // sets the app to run on http://localhost:4200/
+  }
+}
